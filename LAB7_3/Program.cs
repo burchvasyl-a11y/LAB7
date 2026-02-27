515d3ef1fa35bdf6aeada7d7073bbb30816bca66
burchvasyl-a11y <c.burch.vasyl@student.uzhnu.edu.ua>
2026-02-27 16:01:36 +0200

    Add files via upload

---
+++ LAB7_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+    {
+        static void Main()
+        {
+            Func<double, double>[] discounts =
+            {
+                price => price * 0.95,
+                price => price * 0.90,
+                price => price - 100
+            };
+
+            double priceValue = 1000;
+
+            foreach (var discount in discounts)
+            {
+                priceValue = discount(priceValue);
+            }
+
+            Console.WriteLine(priceValue);
+            Console.ReadKey();
+        }
+    }
