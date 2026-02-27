5107a18eb7691a5299248c8ee30aea457d2d9c9e
burchvasyl-a11y <c.burch.vasyl@student.uzhnu.edu.ua>
2026-02-27 15:49:51 +0200

    Add files via upload

---
+++ LAB_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        List<Action> actions = new List<Action>();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            actions.Add(() => Console.WriteLine(i));
+        }
+
+        foreach (var action in actions)
+        {
+            action();
+        }
+        Console.ReadKey();
+    }
+}
