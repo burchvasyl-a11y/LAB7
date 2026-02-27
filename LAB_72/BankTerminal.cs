0c82ee2bede4da135daec64d4b6cd751b96dbc4c
burchvasyl-a11y <c.burch.vasyl@student.uzhnu.edu.ua>
2026-02-27 15:53:21 +0200

    Add files via upload

---
+++ LAB_72/BankTerminal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class BankTerminal
+{
+    public event Action<int> OnMoneyWithdraw;
+    public void Withdraw(int amount)
+    {
+        OnMoneyWithdraw?.Invoke(amount);
+    }
+}
---
+++ LAB_72/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+    {
+        static void Main()
+        {
+            BankTerminal terminal = new BankTerminal();
+            terminal.OnMoneyWithdraw += amount => Console.WriteLine($"Знято {amount} грн");
+            terminal.Withdraw(100);
+            Console.ReadKey();
+        }
+    }
